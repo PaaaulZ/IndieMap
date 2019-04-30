ad344df52494d9b8b12f9f10a395f666fd5284d3
PaaaulZ <skore.2o4@gmail.com>
2019-04-30 23:33:05 +0200

    Better text in the popups

--- IndieMap_py/_website/map.php
+++ IndieMap_py/_website/map.php
@@ -8,20 +8,32 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+	<!-- <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" /> -->
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg==" crossorigin=""></script>
 
 
 	<style>
-		html, body {
+		html, body 
+		{
 			height: 100%;
 			margin: 0;
 		}
-		#map {
-			width: 1024px;
-			height: 768px;
+		#map 
+		{
+			<?php
+
+			if (isMobile())
+			{
+				echo("width: 400px;height: 550px;");
+			}
+			else
+			{
+				echo("width: 1024px;height: 768px;");
+			}
+
+			?>
 		}
 	</style>
 
@@ -29,9 +41,37 @@
 </head>
 <body>
 
-<div id='map'><button type = 'button' onclick = 'javascript:readTextFile("locations.txt");'></button></div>
+<div id='map'></div>
 
 <script>
+
+	var blueIcon = new L.Icon({
+	iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+	});
+
+	var greenIcon = new L.Icon({
+	iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+	});
+
+	var redIcon = new L.Icon({
+	iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+	});
+
 
 	var map = L.map('map').setView([41.91, 12.39], 2);
 
@@ -39,60 +79,61 @@
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	var LeafIcon = L.Icon.extend({
-		options: {
-			shadowUrl: 'leaf-shadow.png',
-			iconSize:     [38, 95],
-			shadowSize:   [50, 64],
-			iconAnchor:   [22, 94],
-			shadowAnchor: [4, 62],
-			popupAnchor:  [-3, -76]
-		}
-	});
-
-	var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
-		redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
-		orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
-
 	<?php
-
-		// GROUP SONGS WITH THE SAME LOCATION
 		
-		$jsonFileContent = file_get_contents('found4map.json');
+		$jsonFileContent = file_get_contents('json/locations.json');
 		$json = json_decode($jsonFileContent);
 
 		$locationArray = array();
+		$countArray = array();
 
 		foreach ($json as &$row)
 		{
-			// Group songs by city
+			// Group songs by city so we can show 1 point on the map and write the songs on the note
 			$cityTMP = $row[0]->city;
 
 			if (!in_array($cityTMP,array_keys($locationArray)))
 			{
+				// If it's the first time that I encounter this city create the array before pushing to avoid errors.
 				$locationArray[$cityTMP] = array();
+				$countArray[$cityTMP] = 0;
 			}
+			// Add this song to the location.
 			array_push($locationArray[$cityTMP],$row);
+			$countArray[$cityTMP]++;
 		}
 
 		// TODO: Change the text shown and beautify code and map.
 		foreach ($locationArray as &$citySong)
 		{
-			$textTMP = "";
+			$textTMP = "<p style = \"text-align:center\"><b>{$citySong[0][0]->city}</b></p><br/>";
 			foreach ($citySong as &$song)
 			{
-				$textTMP = $song[0]->city;
+				// Here we prepare the text that goes in the tooltip		
+				$textTMP .= "<a href = '{$song[0]->lyricsUrl}'>{$song[0]->artist} - {$song[0]->title}<br/></a>";
 			}
-			$textTMP = preg_replace('/[^A-Za-z0-9\-\s]/', '', $textTMP);
-			echo("L.marker([{$song[0]->latitude}, {$song[0]->longitude}]).bindPopup('$textTMP').addTo(map);\n");
+			// Remove special chars in the popup or the map goes boom.
+			$textTMP = preg_replace('/[^A-Za-z0-9\-\s<br\/>\;\"\:\=\.]/', '', $textTMP);
+
+			switch (true) 
+			{
+				case $countArray[$citySong[0][0]->city] >= 5:
+					echo("L.marker([{$song[0]->latitude}, {$song[0]->longitude}], {icon: redIcon}).bindPopup('$textTMP').addTo(map);\n");
+					break;
+				case $countArray[$citySong[0][0]->city] >= 2:
+					echo("L.marker([{$song[0]->latitude}, {$song[0]->longitude}], {icon: greenIcon}).bindPopup('$textTMP').addTo(map);\n");
+					break;
+				default:
+					echo("L.marker([{$song[0]->latitude}, {$song[0]->longitude}], {icon: blueIcon}).bindPopup('$textTMP').addTo(map);\n");	
+					break;
+			}
+		}
+
+		function isMobile() 
+		{
+			return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 		}
 	?>
-
-	// L.marker([41.91, 12.39], {icon: greenIcon}).bindPopup("I am a green leaf.").addTo(map);
-
 </script>
-
-
-
 </body>
 </html>
